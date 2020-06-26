beea800f8b84974838e2eb8ec6babcbcce920960
Alexandra <sandra.larrosa88@gmail.com>
2020-06-26 18:05:19 +0200

    Add variable js form and class html

--- src/js/main.js
+++ src/js/main.js
@@ -31,5 +31,27 @@ const checkPalette3 = document.querySelector('.js-check3');
 
 const formData = {};
 
+//Variables Card
+const textName = document.querySelector('.js-text-name'); 
+const textJob = document.querySelector('.js-text-job');
+const linkEmail = document.querySelector('.js-mail');
+const numberPhone = document.querySelector('.js-number');
+const linkLinkedin = document.querySelector('.js-linkedin');
+const linkGithub = document.querySelector('.js-github');
 
 
+//Variables form
+
+const inputName = document.querySelector('.js-input-name'); 
+const inputJob = document.querySelector('.js-input-job');
+const inputEmail = document.querySelector('.js-input-mail');
+const inputPhone = document.querySelector('.js-input-phone');
+const inputLinkedin = document.querySelector('.js-input-linkedin');
+const inputGithub = document.querySelector('.js-input-github');
+
+
+
+
+//Crear variables de los input. Recoger los datos
+
+//
