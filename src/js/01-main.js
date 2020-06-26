fb28e893a4dd8b42062a08ed3122eb49cadcaf6a
Alexandra <sandra.larrosa88@gmail.com>
2020-06-26 12:47:51 +0200

    partials js

---
+++ src/js/01-main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+/* VARIABLES */
+
+//Pasar a CSS creando una clase con los colores de cada paleta
+/* .palette1 {
+    color:
+    & h1 {
+        color: 
+    }
+    & p{
+        color:
+    }
+} */
+
+const palette1 = document.querySelector('.js-palette1');
+const palette2 = document.querySelector('.js-palette2');
+const palette3 = document.querySelector('.js-palette3');
+
+
+
+//FORMULARIO
+//Check Paleta de colores
+const checkPalette1 = document.querySelector('.js-check1');
+const checkPalette2 = document.querySelector('.js-check2');
+const checkPalette3 = document.querySelector('.js-check3');
+
+//Datos Formulario
+
+const formData = {};
+
+
