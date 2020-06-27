cac520588163a5701c3b41379a258599669e7f04
Alexandra <sandra.larrosa88@gmail.com>
2020-06-27 09:38:42 +0200

    card customization ok

--- src/js/main.js
+++ src/js/main.js
@@ -30,13 +30,16 @@ const formData = {};
 
 //Objeto que tiene todas las clases de la card para poder pintarlas
 const cardFields = {
-  fullname: document.querySelector(".js-text-name"),
-  job: document.querySelector(".js-text-job"),
-  emailAddress: document.querySelector(".js-mail"),
-  telephone: document.querySelector(".js-number"),
-  linkedin: document.querySelector(".js-linkedin"),
-  github: document.querySelector(".js-github"),
+    fullname: document.querySelector(".js-text-name"),
+    job: document.querySelector(".js-text-job"),
+    emailAddress: document.querySelector(".js-mail"),
+    telephone: document.querySelector(".js-number"),
+    linkedin: document.querySelector(".js-linkedin"),
+    github: document.querySelector(".js-github"),
+    name: document.querySelector('.js-share')
 };
+
+console.log(cardFields.name.disabled);
 
 //Variables con input del form
 const inputName = document.querySelector(".js-input-name");
@@ -48,11 +51,19 @@ const inputGithub = document.querySelector(".js-input-github");
 
 //Funcion que guarda los datos en el objeto y lo pinta en la tarjeta
 function setData(ev) {
-  const name = ev.currentTarget.name;
-  const inputValue = ev.currentTarget.value;
+    const name = ev.currentTarget.name;
+    const inputValue = ev.currentTarget.value;
 
-  formData[name] = inputValue;
-  cardFields[name].innerHTML = ev.currentTarget.value;
+    formData[name] = inputValue;
+    cardFields[name].innerHTML = ev.currentTarget.value;
+}
+
+function changeLinks(ev) {
+    const name = ev.currentTarget.name;
+    const inputValue = ev.currentTarget.value;
+
+    formData[name] = inputValue;
+    cardFields[name].href = ev.currentTarget.value;
 }
 
 //Funcion que valida los datos
@@ -60,6 +71,11 @@ function setData(ev) {
 //Listeners
 inputName.addEventListener("keyup", setData);
 inputJob.addEventListener("keyup", setData);
+inputEmail.addEventListener('keyup', changeLinks);
+inputPhone.addEventListener('keyup', changeLinks);
+inputLinkedin.addEventListener('keyup', changeLinks);
+inputGithub.addEventListener('keyup', changeLinks);
+
 
 //-------------------RESET------------------------
 // get resetElement dom element
